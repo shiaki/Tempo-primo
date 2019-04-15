f661135b7a4051c22accfef1b0f3c5a9deb2f240
Yujing Qin <qinyj.astro@gmail.com>
2019-04-15 16:47:07 -0700

    .

--- zcorr/zcorr.py
+++ zcorr/zcorr.py
@@ -3,15 +3,20 @@
 '''
     Peculiar velocity correction for nearby galaxies using Carrick+15.
 
-    TODO: (YQ, 04/14)
+    TODO:
         disclaimer,
         warning for multiple values,
         fine-tuning root finding.
+        (YQ, 04/14)
+
+        different measurements of redshift. (v3k, heliocentric, etc.)
+        (YQ, 04/15)
 '''
 
 import os
 import numpy as np
-from scipy.interpolate import RegularGridInterpolator
+from scipy.interpolate import RegularGridInterpolator, interp1d
+from scipy.optimize import newton
 import astropy.coordinates as crd
 
 def galactic_Rn(ra, dec):
@@ -50,12 +55,46 @@ class ZcorrClass(object):
 
         # write other parameters
         self._h = h
+        self._c = 2.99792458e5 # in km/s
         self._hsq = h ** 2
+        self._R_ax = np.linspace(0., 200., 512)
 
-    def zcorr(self, ra, dec, z_obs):
-        pass # TODO
+    def _dc(ra, dec, z_obs,):
 
-    def dist(self, ra, dec, z_obs):
-        pass # TODO
+        # otherwise: calculate V_pec corrected redshift.
+        cx, cy, cz = galactic_Rn(ra, dec)
+        xi_cart = np.outer(self._R_ax, [cx, cy, cz]) # GC, cartesian, Mpc/h
+        Vp_R = self._Vr_pec_intp(xi_cart) # Vr_pec along xi_cart (LOS)
+
+        # root-finding.
+        v_obs = self._c * z_obs
+        vrp_intp = interp1d(self._R_ax, Vp_R, fill_value='extrapolate')
+        rfc = lambda d: v_obs - (1.e2 * self._hsq) * d - vrp_intp(d)
+        d_opt = newton(rfc, z_obs * self._c / (100. * (h ** 2)),)
+
+        return d_opt
+
+    def zcorr(self, ra, dec, z_obs,): # ** assuming z_obs in cmb frame.
+
+        ''' Peculiar velocity-corrected redshift. '''
+
+        # beyond the range: return original z.
+        if z_obs > 2.e2 * (1.e2 * self._hsq) / self._c:
+            return z_obs # about 0.03
+
+        d_opt = self._dc(ra, dec, z_obs)
+        return d_opt * (1.e2 * self._hsq / self._c)
+
+    def dist(self, ra, dec, z_obs,):
+
+        ''' Peculiar velocity corrected distance. '''
+
+        # beyond the range: return original z.
+        if z_obs > 2.e2 * (1.e2 * self._hsq) / self._c:
+            return z_obs * self._c / (1.e2 * self._h)
+            # ** only works for local universe
+
+        d_opt = self._dc(ra, dec, z_obs)
+        return d_opt * self._h
 
 Zcorr = ZcorrClass()
